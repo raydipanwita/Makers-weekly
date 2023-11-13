c7f5ea43e75e7cd3a429a02eb3a0a410fb911538
Dipanwita <rite2dipa@gmail.com>
2023-11-13 15:56:07 +0000

    Tests for add five

--- lib/add_five.py
+++ lib/add_five.py
@@ -3,9 +3,3 @@ def add_five(num):
     num_after_adding = num + 5
     print(f"I have calculated {num} + 5 = {num_after_adding}")
     return num_after_adding
-
-result = add_five(23)
-result1 = add_five(result)
-result2 = add_five(result1)
-print(result)
-
