fb9bda74dba943918765dfdf9b6eebb583050539
Dipanwita <rite2dipa@gmail.com>
2023-11-14 11:38:59 +0000

    updated with - Exception error

--- lib/reminder.py
+++ lib/reminder.py
@@ -1,9 +1,12 @@
 class Reminder():
     def __init__(self, name):
         self.name = name
+        self.task = None
 
     def remind_me(self, task):
         self.task = task
 
     def remind(self):
+        if self.task is None:                 # We want to fail if there is no reminder yet.
+            raise Exception("No reminder set!")
         return f"{self.task}, {self.name}!"
