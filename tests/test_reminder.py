4b5ec0784b073f86458defcf403b944db7b8ce7f
Dipanwita <rite2dipa@gmail.com>
2023-11-14 11:39:17 +0000

    updated with - Exception error

--- tests/test_reminder.py
+++ tests/test_reminder.py
@@ -1,10 +1,12 @@
+import pytest
 from lib.reminder import *
 
 def test_reminds_the_user_to_do_a_task_kay():
     reminder = Reminder("Kay")
-    reminder.remind_me("Walk the dog")
-    result = reminder.remind()
-    assert result == "Walk the dog, Kay!"
+    with pytest.raises(Exception)as e:
+        reminder.remind()
+    error_message = str(e.value)
+    assert error_message == "No reminder set!"
 
 def test_reminds_the_user_to_do_a_task_kate():
     reminder = Reminder("Kate")
